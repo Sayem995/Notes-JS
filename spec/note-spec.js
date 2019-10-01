4d190e3e9170abd8c58688bc22ae27139f38ae7b
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-01 14:55:11 +0100

    initial commit, file set-up, first test

---
+++ spec/note-spec.js
@@ -0,0 +1,11 @@
+function testNoteTextDefault(){
+  try {
+    var note = new Note();
+    assert.isTrue(note.getText() === "My favourite language is JavaScript");
+  }
+    catch(err) {
+      return "Error - " + arguments.callee.name + " : " + err;
+    }
+
+    return "Pass - " + arguments.callee.name;
+  };
