6abc6b05a09c4587d14b3633b3609138bd26e04b
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-04 10:47:08 +0100

    ccreate note list view, passing in the note list model. create note-controller-spec.js file

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,4 +1,18 @@
-function NoteController(){
-console.log(document.getElementById("app").innerHTML = "howdy");
+(function(exports) {
+  function NoteController(NoteList) {
+    console.log(NoteList);
+
+    var notelist = new NoteList;
+    notelist.create("Favourite drink: seltzer");
+    console.log(notelist);
+
+    var notelistview = new NoteListView;
+    notelistview.returnHtml(notelist);
+
+    console.log(notelistview.returnHtml(notelist));
+
+console.log(document.getElementById("app").innerHTML = notelistview.returnHtml(notelist));
 };
-NoteController();
+exports.NoteController = NoteController;
+})(this);
+NoteController(NoteList);
